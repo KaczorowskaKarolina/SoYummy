635ae536a0e1cb390dfbc27ebc9fa99fffcbeda8
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-14 16:24:58 +0100

    Created component CurvedButton and added comments on how to use it.

---
+++ src/components/Atoms/CurvedButton/CurvedButton.jsx
@@ -0,0 +1,24 @@
+import styles from './CurvedButton.module.css';
+
+export const Button = ({ title, type = 'button', isGreen = false, onClick = () => {} }) => {
+  const classes = `${styles.CurvedButton} ${isGreen && styles.green}`;
+  return (
+    <button className={classes} type={type} onClick={onClick}>
+      {title}
+    </button>
+  );
+};
+
+// title is a text that appears on button f.ex. 'Submit'
+
+// type prop is a type of a button and a string. By default it's just a button
+// but if you need it to be some other type
+// just add attribute type whith the type you want
+// when decalring component
+
+// Button is by default transparent,
+// so if you need it to be green add
+// attribute isGreen with boolean value true
+// when decalring component
+
+// onClick is obvious. By default this prop is an empty function so
